c2a90c5c82e7a1addce96f9eca529eadbeb9e8bf
jabier.martinez <jabimail@gmail.com>
2014-10-27 22:57:06 +0100

    images adapter improvements

--- org.but4reuse.adapters.images/src/org/but4reuse/adapters/images/ImageAdapter.java
+++ org.but4reuse.adapters.images/src/org/but4reuse/adapters/images/ImageAdapter.java
@@ -1,63 +1,32 @@
 package org.but4reuse.adapters.images;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.but4reuse.adapters.IAdapter;
 import org.but4reuse.adapters.IElement;
+import org.but4reuse.adapters.images.utils.ImageUtils;
 import org.but4reuse.utils.files.FileUtils;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.RGB;
-import org.eclipse.swt.widgets.Display;
 
 /**
  * Image adapter
+ * 
  * @author jabier.martinez
  */
 public class ImageAdapter implements IAdapter {
-	public static final String[] IMAGE_EXTENSIONS = { "*.gif", "*.png", "*.bmp", "*.jpg" };
-
-	public static boolean isImageFile(String fileName) {
-		int dot = fileName.lastIndexOf(".");
-		if (dot == -1) {
-			return false;
-		} else {
-			String fileExt = fileName.substring(dot, fileName.length());
-			for (String extension : IMAGE_EXTENSIONS) {
-				// remove *
-				extension = extension.substring(1);
-				if (fileExt.equalsIgnoreCase(extension)) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
-	public static Image getImage(String absolutePath) {
-		try {
-			if (absolutePath == null) {
-				return null;
-			}
-			return new Image(Display.getDefault(), new FileInputStream(absolutePath));
-		} catch (Exception e) {
-			// If any exception happens return null
-			return null;
-		}
-	}
 
 	@Override
 	public boolean isAdaptable(URI uri, IProgressMonitor monitor) {
 		File file = FileUtils.getFile(uri);
-		if (file != null && file.exists() && !file.isDirectory() && isImageFile(file.getName())) {
+		if (file != null && file.exists() && !file.isDirectory() && ImageUtils.isImageFile(file.getName())) {
 			return true;
 		}
 		return false;
@@ -67,7 +36,7 @@ public class ImageAdapter implements IAdapter {
 	public List<IElement> adapt(URI uri, IProgressMonitor monitor) {
 		List<IElement> elements = new ArrayList<IElement>();
 		File file = FileUtils.getFile(uri);
-		Image image = getImage(file.getAbsolutePath());
+		Image image = ImageUtils.getImage(file.getAbsolutePath());
 		ImageData imageData = image.getImageData();
 
 		for (int y = 0; y < imageData.height; y++) {
@@ -95,13 +64,45 @@ public class ImageAdapter implements IAdapter {
 
 	@Override
 	public void construct(URI uri, List<IElement> elements, IProgressMonitor monitor) {
-		// TODO construct image
-		Display.getDefault().asyncExec(new Runnable() {
-			@Override
-			public void run() {
-				MessageDialog.openInformation(Display.getDefault().getActiveShell(), "Construction",
-						"For the moment, construction is not available");
+		try {
+			// Use the given file or use a default name if a folder was given
+			if (uri.toString().endsWith("/")) {
+				uri = new URI(uri.toString() + "construct.png");
 			}
-		});
+			// Create file if it does not exist
+			File file = FileUtils.getFile(uri);
+			FileUtils.createFile(file);
+			
+			// Calculate max width and max height
+			int maxWidth = -1;
+			int maxHeight = -1;
+			for(IElement element : elements){
+				if(element instanceof PixelElement){
+					PixelElement pElement = (PixelElement) element;
+					if(maxWidth < pElement.position.x){
+						maxWidth = pElement.position.x;
+					}
+					if(maxHeight < pElement.position.y){
+						maxHeight = pElement.position.y;
+					}
+				}
+			}
+			
+			// Start creating the image
+			int IMAGE_DEPTH = 32;
+			ImageData imageData = ImageUtils.createEmptyDirectPaletteImageData(maxWidth + 1, maxHeight + 1, IMAGE_DEPTH);
+			for(IElement element : elements){
+				if(element instanceof PixelElement){
+					PixelElement pElement = (PixelElement) element;
+					imageData.setAlpha(pElement.position.x, pElement.position.y, pElement.alpha);
+					imageData.setPixel(pElement.position.x, pElement.position.y, pElement.color.hashCode());
+				}
+			}
+			
+			// Save it
+			ImageUtils.saveImageToFile(imageData, file.getAbsolutePath(), SWT.IMAGE_PNG);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
