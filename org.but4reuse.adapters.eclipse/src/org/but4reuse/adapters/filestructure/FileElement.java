81920193e37a001daff4c0b7c473e890cdbfdba7
P42k <josephkratos@gmail.com>
2014-11-07 14:05:17 +0100

    création du dossier org.but4reuse.adapters.eclipse

---
+++ org.but4reuse.adapters.eclipse/src/org/but4reuse/adapters/filestructure/FileElement.java
@@ -0,0 +1,84 @@
+package org.but4reuse.adapters.filestructure;
+
+import java.net.URI;
+
+import org.but4reuse.adapters.IElement;
+import org.but4reuse.adapters.impl.AbstractElement;
+import org.but4reuse.adapters.markers.IMarkerElement;
+import org.but4reuse.utils.workbench.WorkbenchUtils;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.runtime.CoreException;
+
+/**
+ * File Element
+ * @author jabier.martinez
+ */
+public class FileElement extends AbstractElement implements IMarkerElement {
+
+	private URI uri;
+	private URI relativeURI;
+
+	@Override
+	public double similarity(IElement anotherElement) {
+		// When they have the same relative URI
+		// TODO add a preference option to check file content
+		if (anotherElement instanceof FileElement) {
+			if (((FileElement) anotherElement).getRelativeURI().equals(getRelativeURI())) {
+				return 1;
+			}
+		}
+		return 0;
+	}
+
+	
+	@Override
+	public String getText() {
+		return getRelativeURI().toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((getRelativeURI() == null) ? 0 : getRelativeURI().hashCode());
+		return result;
+	}
+
+
+	public URI getRelativeURI() {
+		return relativeURI;
+	}
+
+
+	public void setRelativeURI(URI relativeURI) {
+		this.relativeURI = relativeURI;
+	}
+
+	@Override
+	public IMarker getMarker() {
+		IMarker marker = null;
+		IFile ifile = WorkbenchUtils.getIFileFromURI(getUri());
+		if (ifile != null && ifile.exists()) {
+			try {
+				marker = ifile.createMarker(IMarker.TEXT);
+				marker.setAttribute(IMarker.LOCATION, ifile.getName());
+				//marker.setAttribute(IMarker.LINE_NUMBER, lineNumber);
+			} catch (CoreException e) {
+				e.printStackTrace();
+			}
+		}
+		return marker;
+	}
+
+
+	public URI getUri() {
+		return uri;
+	}
+
+
+	public void setUri(URI uri) {
+		this.uri = uri;
+	}
+
+}
---
+++ org.but4reuse.adapters.eclipse/src/org/but4reuse/adapters/filestructure/FolderElement.java
@@ -0,0 +1,8 @@
+package org.but4reuse.adapters.filestructure;
+
+
+/**
+ * Folder Element
+ * @author jabier.martinez
+ */
+public class FolderElement extends FileElement {}
