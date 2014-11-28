29dd315d365c89e601019355961b71970e93bbca
jabier.martinez <jabimail@gmail.com>
2014-11-28 19:51:50 +0100

    Construct for Graphs Adapter

--- org.but4reuse.adapters.graphs/src/org/but4reuse/adapters/graphs/GraphsAdapter.java
+++ org.but4reuse.adapters.graphs/src/org/but4reuse/adapters/graphs/GraphsAdapter.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,8 +15,6 @@ import org.but4reuse.adapters.IAdapter;
 import org.but4reuse.adapters.IElement;
 import org.but4reuse.utils.files.FileUtils;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.widgets.Display;
 
 import com.tinkerpop.blueprints.Direction;
 import com.tinkerpop.blueprints.Edge;
@@ -23,6 +22,7 @@ import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
 import com.tinkerpop.blueprints.util.io.graphml.GraphMLReader;
+import com.tinkerpop.blueprints.util.io.graphml.GraphMLWriter;
 
 /**
  * Graphs Adapter
@@ -61,7 +61,7 @@ public class GraphsAdapter implements IAdapter {
 			e.printStackTrace();
 		}
 
-		HashMap<Vertex,VertexElement> map = new HashMap<Vertex,VertexElement>();
+		HashMap<Vertex, VertexElement> map = new HashMap<Vertex, VertexElement>();
 		Iterable<Vertex> vertices = graph.getVertices();
 		for (Vertex vertex : vertices) {
 			VertexElement v = new VertexElement();
@@ -77,9 +77,9 @@ public class GraphsAdapter implements IAdapter {
 			elements.add(e);
 			// Add dependencies
 			Vertex from = edge.getVertex(Direction.OUT);
-			e.addDependency("from",map.get(from));
+			e.addDependency("from", map.get(from));
 			Vertex to = edge.getVertex(Direction.IN);
-			e.addDependency("to",map.get(to));
+			e.addDependency("to", map.get(to));
 		}
 
 		return elements;
@@ -87,14 +87,73 @@ public class GraphsAdapter implements IAdapter {
 
 	@Override
 	public void construct(URI uri, List<IElement> elements, IProgressMonitor monitor) {
-		// TODO construct
-		Display.getDefault().asyncExec(new Runnable() {
-			@Override
-			public void run() {
-				MessageDialog.openInformation(Display.getDefault().getActiveShell(), "Construction",
-						"For the moment, construction is not available for the Graphs adapter");
+
+		// Create graph
+		Graph graph = new TinkerGraph();
+
+		// Add vertices
+		for (IElement element : elements) {
+			if (element instanceof VertexElement) {
+				VertexElement ve = (VertexElement) element;
+				Vertex v = graph.addVertex(ve.getVertex().getId());
+				// adding all its properties
+				for(String key : ve.getVertex().getPropertyKeys()){
+					v.setProperty(key, ve.getVertex().getProperty(key));
+				}
 			}
-		});
+		}
+		
+		// Add edges
+		// We create link vertices when they do not exist in the graph but the edge reference to it
+		int linkVertex = 0;
+		for (IElement element : elements) {
+			if (element instanceof EdgeElement) {
+				EdgeElement ee = (EdgeElement) element;
+				Vertex v1 = ee.getEdge().getVertex(Direction.IN);
+				Vertex v2 = ee.getEdge().getVertex(Direction.OUT);
+				// If they dont exist, create fake ones
+				if(graph.getVertex(v1.getId())==null){
+					v1 = graph.addVertex("link vertex "+ linkVertex);
+					v1.setProperty("label", v1.getId());
+					linkVertex++;
+				} else {
+					// get the current object in the graph
+					v1 = graph.getVertex(v1.getId());
+				}
+				if(graph.getVertex(v2.getId())==null){
+					v2 = graph.addVertex("link vertex "+ linkVertex);
+					v2.setProperty("label", v2.getId());
+					linkVertex++;
+				} else {
+					// get the current object in the graph
+					v2 = graph.getVertex(v2.getId());
+				}
+				
+				Edge edge = graph.addEdge(ee.getEdge().getId(), v1, v2, ee.getEdge().getLabel());
+				// adding all its properties
+				for(String key : ee.getEdge().getPropertyKeys()){
+					edge.setProperty(key, ee.getEdge().getProperty(key));
+				}
+			}
+		}
+
+		// Save
+		GraphMLWriter writer = new GraphMLWriter(graph);
+		writer.setNormalize(true);
+
+		try {
+			// Use the given file or use a default name if a folder was given
+			if (uri.toString().endsWith("/")) {
+				uri = new URI(uri.toString() + "graph.graphml");
+			}
+			// Create file if it does not exist
+			File file = FileUtils.getFile(uri);
+			FileUtils.createFile(file);
+
+			writer.outputGraph(file.getAbsolutePath());
+		} catch (IOException | URISyntaxException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
