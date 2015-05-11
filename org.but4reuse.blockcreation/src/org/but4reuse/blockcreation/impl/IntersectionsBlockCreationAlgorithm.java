7d0cadba805d713f892bd6e471dbdf785b0db98c
jabier.martinez <jabimail@gmail.com>
2015-05-11 13:09:31 +0200

    Keep order of IElements on Blocks

--- org.but4reuse.blockcreation/src/org/but4reuse/blockcreation/impl/IntersectionsBlockCreationAlgorithm.java
+++ org.but4reuse.blockcreation/src/org/but4reuse/blockcreation/impl/IntersectionsBlockCreationAlgorithm.java
@@ -2,6 +2,7 @@ package org.but4reuse.blockcreation.impl;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -29,8 +30,8 @@ public class IntersectionsBlockCreationAlgorithm implements IBlockCreationAlgori
 
 		// In R we will have, for each element, the indexes of the artefacts
 		// where they appear
-		// TODO check with LinkedHashMap to maintain elements order
-		Map<IElement, List<Integer>> R = new HashMap<IElement, List<Integer>>();
+		// LinkedHashMap to maintain elements order
+		Map<IElement, List<Integer>> R = new LinkedHashMap<IElement, List<Integer>>();
 
 		// A map from IElement to the IElementWrappers that contains similar
 		// IElement
