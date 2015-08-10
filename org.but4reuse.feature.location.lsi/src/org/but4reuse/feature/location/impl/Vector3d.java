3537bf7dc4c4368e18f9b2208d293bb8b2e05788
J-Art75 <ar_joanny@hotmail.fr>
2015-08-10 14:27:19 +0200

    Format + Comments

--- org.but4reuse.feature.location.lsi/src/org/but4reuse/feature/location/impl/Vector3d.java
+++ org.but4reuse.feature.location.lsi/src/org/but4reuse/feature/location/impl/Vector3d.java
@@ -1,15 +1,13 @@
 package org.but4reuse.feature.location.impl;
 
 public class Vector3d {
-	
-	public double x,y,z;
-	
-	public Vector3d(double x, double y,double z)
-	{
+
+	public double x, y, z;
+
+	public Vector3d(double x, double y, double z) {
 		this.x = x;
 		this.y = y;
-		this.z=z;
+		this.z = z;
 	}
-	
 
 }
