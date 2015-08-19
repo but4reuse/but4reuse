e8014cc0d14c7f7fd68f842b48492e7040ae1f44
J-Art75 <ar_joanny@hotmail.fr>
2015-08-19 13:52:35 +0200

    format

--- org.but4reuse.utils/src/org/but4reuse/utils/ui/preferences/ScaleLabel.java
+++ org.but4reuse.utils/src/org/but4reuse/utils/ui/preferences/ScaleLabel.java
@@ -12,39 +12,36 @@ import org.eclipse.swt.widgets.Text;
 
 public class ScaleLabel {
 
-	
-	public enum Type{
-		INT,DOUBLE;
+	public enum Type {
+		INT, DOUBLE;
 	}
+
 	private Scale scaleC;
 	private Text textC;
 	private double value;
 	private double minVal;
 	private double maxVal;
-	private Type typeC; 
-	
-	public ScaleLabel(Scale scale,Text text, Type type, double min,double max) {
-		
+	private Type typeC;
+
+	public ScaleLabel(Scale scale, Text text, Type type, double min, double max) {
+
 		scaleC = scale;
 		textC = text;
 		minVal = min;
 		maxVal = max;
 		typeC = type;
 		scaleC.setIncrement(1);
-		if(typeC == Type.INT)
-		{
-			scale.setMinimum((int)min);		
-			scale.setMaximum((int)max);
+		if (typeC == Type.INT) {
+			scale.setMinimum((int) min);
+			scale.setMaximum((int) max);
 			scaleC.setPageIncrement(50);
-		}
-		else if(typeC ==  Type.DOUBLE)
-		{
-			scale.setMinimum((int)min*100);
-			scale.setMaximum((int)max*100);
-			scaleC.setPageIncrement((int)(min+max)/10);
+		} else if (typeC == Type.DOUBLE) {
+			scale.setMinimum((int) min * 100);
+			scale.setMaximum((int) max * 100);
+			scaleC.setPageIncrement((int) (min + max) / 10);
 		}
 		scaleC.addSelectionListener(new SelectionListener() {
-			
+
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				/*
@@ -52,19 +49,19 @@ public class ScaleLabel {
 				 * text control
 				 */
 				Scale s = (Scale) e.getSource();
-				value = s.getSelection() ;
-				if(typeC == Type.INT)
-					textC.setText(value+"");
+				value = s.getSelection();
+				if (typeC == Type.INT)
+					textC.setText(value + "");
 				else
-					textC.setText(String.format("%.2f",value));
+					textC.setText(String.format("%.2f", value));
 			}
 
 			@Override
 			public void widgetDefaultSelected(SelectionEvent e) {
 
 			}
-		});				
-		
+		});
+
 		textC.addVerifyListener(new VerifyListener() {
 
 			@Override
@@ -78,17 +75,15 @@ public class ScaleLabel {
 				if ((int) e.character == 8) // It means backspace char so we
 											// remove the last char
 				{
-					int  ind = textC.getCaretPosition();
-					
-					if(ind == 0)
+					int ind = textC.getCaretPosition();
+
+					if (ind == 0)
 						return;
-					if(ind == value_t.length()){
+					if (ind == value_t.length()) {
 						value_t = value_t.substring(0, value_t.length() - 1);
-					}
-					else
-					{
-						String tmp = value_t.substring(0, ind-1);
-						value_t = tmp+value_t.substring(ind,value_t.length());
+					} else {
+						String tmp = value_t.substring(0, ind - 1);
+						value_t = tmp + value_t.substring(ind, value_t.length());
 					}
 					textC.setText(value_t);
 					return;
@@ -127,11 +122,11 @@ public class ScaleLabel {
 							String st = String.format("%.2f", value);
 							st = st.replace(",", ".");
 
-							if(typeC == Type.DOUBLE)
+							if (typeC == Type.DOUBLE)
 								scaleC.setSelection((int) (value * 100));
-							if(typeC == Type.INT)
-								scaleC.setSelection((int)value);
-							
+							if (typeC == Type.INT)
+								scaleC.setSelection((int) value);
+
 							e.doit = true;
 						} catch (NumberFormatException ex) {
 							MessageBox mb = new MessageBox(Display.getDefault().getActiveShell(), SWT.OK);
@@ -140,7 +135,7 @@ public class ScaleLabel {
 							mb.setText("Error value");
 							mb.setMessage("You must select a number");
 							mb.open();
-							textC.setText((minVal+maxVal)/2+"");
+							textC.setText((minVal + maxVal) / 2 + "");
 						}
 					}
 
@@ -150,21 +145,18 @@ public class ScaleLabel {
 						 * Here it's for when we update the Text control with
 						 * setText method we check if the number is between 0
 						 * and 1
-						 * 
 						 */
 
-					
-						
-						e.text= e.text.replace(',' ,'.');
+						e.text = e.text.replace(',', '.');
 						double d = Double.parseDouble(e.text);
-						
-						if (d < minVal) 
+
+						if (d < minVal)
 							d = minVal;
 						else if (d > maxVal)
 							d = maxVal;
-			
-						if(typeC == Type.DOUBLE)
-							d/=100;
+
+						if (typeC == Type.DOUBLE)
+							d /= 100;
 						e.text = String.format("%.2f", d);
 
 						e.doit = true;
@@ -175,40 +167,36 @@ public class ScaleLabel {
 						mb.setText("Error value");
 						mb.setMessage("You must select a number");
 						mb.open();
-						textC.setText((minVal+maxVal)/2+"");
+						textC.setText((minVal + maxVal) / 2 + "");
 					}
 				}
 			}
 		});
-		
+
 	}
 
-	public void change(Type type, double min, double max)
-	{
+	public void change(Type type, double min, double max) {
 		minVal = min;
 		maxVal = max;
 		typeC = type;
-		
-		if(typeC == Type.INT)
-		{
+
+		if (typeC == Type.INT) {
 			scaleC.setPageIncrement(50);
-			value = (int)((minVal + maxVal) /2);
+			value = (int) ((minVal + maxVal) / 2);
+		} else if (typeC == Type.DOUBLE) {
+			minVal *= 100;
+			maxVal *= 100;
+			value = (minVal + maxVal) / 2;
+			scaleC.setPageIncrement((int) (minVal + maxVal) / 10);
 		}
-		else if(typeC == Type.DOUBLE)
-		{
-			minVal *=100;
-			maxVal *=100;
-			value = (minVal + maxVal) /2;
-			scaleC.setPageIncrement((int)(minVal+maxVal)/10);
-		}
-		
-		scaleC.setMinimum((int)minVal);
-		scaleC.setMaximum((int)maxVal);
-		scaleC.setSelection((int)(minVal + maxVal)  / 2);
-		scaleC.setSelection((int)value);
-		
-		textC.setText(value+"");
+
+		scaleC.setMinimum((int) minVal);
+		scaleC.setMaximum((int) maxVal);
+		scaleC.setSelection((int) (minVal + maxVal) / 2);
+		scaleC.setSelection((int) value);
+
+		textC.setText(value + "");
 
 	}
-	
+
 }
