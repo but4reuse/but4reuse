3c55688ed6af2b40d3850077d0df9516f05edbd9
jabier.martinez <jabimail@gmail.com>
2016-03-31 13:36:21 +0200

    similar elements block identification test and fix

--- tests/org.but4reuse.block.identification.tests/src/org/but4reuse/block/identification/impl/SimilarElementsBlockIdentificationTest.java
+++ tests/org.but4reuse.block.identification.tests/src/org/but4reuse/block/identification/impl/SimilarElementsBlockIdentificationTest.java
@@ -17,12 +17,12 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * Test for Intersections block identification (Interdependent elements algo)
+ * Test for similar elements block identification
  * 
  * @author jabier.martinez
  * 
  */
-public class IntersectionsBlockIdentificationTest {
+public class SimilarElementsBlockIdentificationTest {
 
 	/**
 	 * We create test 3 artefacts with shared elements and we check that the
@@ -32,7 +32,7 @@ public class IntersectionsBlockIdentificationTest {
 	public void blocksIdentified() {
 
 		// Prepare
-		IBlockIdentification algo = new IntersectionsBlockIdentification();
+		IBlockIdentification algo = new SimilarElementsBlockIdentification();
 		List<AdaptedArtefact> adaptedArtefacts = new ArrayList<AdaptedArtefact>();
 		AdaptedArtefact a1 = AdaptedModelHelper.wrapElementsToCreateAdaptedArtefact(TestElementsCreator
 				.createElements(10));
@@ -45,38 +45,26 @@ public class IntersectionsBlockIdentificationTest {
 		adaptedArtefacts.add(a3);
 		List<Block> blocks = algo.identifyBlocks(adaptedArtefacts, new NullProgressMonitor());
 
-		// 3 blocks: one common and another two
-		Assert.assertEquals(3, blocks.size());
+		// 100 blocks
+		Assert.assertEquals(100, blocks.size());
 
 		AdaptedModel adaptedModel = AdaptedModelFactory.eINSTANCE.createAdaptedModel();
 		adaptedModel.getOwnedAdaptedArtefacts().addAll(adaptedArtefacts);
 		adaptedModel.getOwnedBlocks().addAll(blocks);
 
-		// Check that there is only one common
-		Assert.assertEquals(1, AdaptedModelHelper.getCommonBlocks(adaptedModel).size());
+		// Check the common blocks
+		Assert.assertEquals(10, AdaptedModelHelper.getCommonBlocks(adaptedModel).size());
 
-		// Check that size is ok and that order is always the same
-		Assert.assertEquals(10, blocks.get(0).getOwnedBlockElements().size());
-		Assert.assertEquals(20, blocks.get(1).getOwnedBlockElements().size());
-		Assert.assertEquals(70, blocks.get(2).getOwnedBlockElements().size());
+		// Check that size is ok, all should be size 1. Check also that order is
+		// correct
+		int i = 0;
+		for (Block b : blocks) {
+			Assert.assertEquals(1, b.getOwnedBlockElements().size());
+			List<IElement> elements = AdaptedModelHelper.getElementsOfBlock(b);
+			Assert.assertEquals(i, ((TestElement) elements.get(0)).id);
+			i++;
+		}
 
-		// Check that they are the correct elements and that elements are in the
-		// correct order
-		// Block 0, elements from 0 to 9
-		List<IElement> elements = AdaptedModelHelper.getElementsOfBlock(blocks.get(0));
-		for (int i = 0; i < elements.size(); i++) {
-			Assert.assertEquals(i, ((TestElement) elements.get(i)).id);
-		}
-		// Block 1, elements from 10 to 29
-		elements = AdaptedModelHelper.getElementsOfBlock(blocks.get(1));
-		for (int i = 0; i < elements.size(); i++) {
-			Assert.assertEquals(i + 10, ((TestElement) elements.get(i)).id);
-		}
-		// Block 2, elements from 30 to 99
-		elements = AdaptedModelHelper.getElementsOfBlock(blocks.get(2));
-		for (int i = 0; i < elements.size(); i++) {
-			Assert.assertEquals(i + 30, ((TestElement) elements.get(i)).id);
-		}
 	}
 
 }
