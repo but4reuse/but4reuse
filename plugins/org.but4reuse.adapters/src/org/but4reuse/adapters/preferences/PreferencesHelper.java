1ce6ff38a1e891877e62a3e254cf337f8ffaeb74
jabiercoding <jabiercoding@gmail.com>
2022-01-28 11:56:52 +0100

    refactoring to enable b4r usage in a Main method

--- plugins/org.but4reuse.adapters/src/org/but4reuse/adapters/preferences/PreferencesHelper.java
+++ plugins/org.but4reuse.adapters/src/org/but4reuse/adapters/preferences/PreferencesHelper.java
@@ -12,8 +12,16 @@ import org.eclipse.jface.preference.IPreferenceStore;
  */
 public class PreferencesHelper {
 
-	static IEclipsePreferences prefs = InstanceScope.INSTANCE
-			.getNode(Activator.getDefault().getBundle().getSymbolicName());
+	static IEclipsePreferences prefs;
+
+	// static initialization
+	static {
+		if (Activator.getDefault() != null) {
+			prefs = InstanceScope.INSTANCE.getNode(Activator.getDefault().getBundle().getSymbolicName());
+		} else {
+			prefs = null;
+		}
+	}
 
 	public static final String AUTOMATIC_EQUAL_THRESHOLD = "automatic_threshold";
 	public static final String ASK_USER_THRESHOLD = "ask_user_threshold";
@@ -31,18 +39,30 @@ public class PreferencesHelper {
 	}
 
 	public static double getAutomaticEqualThreshold() {
+		if (prefs == null) {
+			return 1.00;
+		}
 		return prefs.getDouble(AUTOMATIC_EQUAL_THRESHOLD, 1.00);
 	}
 
 	public static double getManualEqualThreshold() {
+		if (prefs == null) {
+			return 0.9;
+		}
 		return prefs.getDouble(ASK_USER_THRESHOLD, 0.9);
 	}
 
 	public static boolean isManualEqualActivated() {
+		if (prefs == null) {
+			return false;
+		}
 		return prefs.getBoolean(ASK_USER, false);
 	}
 
 	public static void setManualEqual(boolean value) {
+		if (prefs == null) {
+			return;
+		}
 		prefs.putBoolean(ASK_USER, value);
 	}
 
@@ -51,14 +71,23 @@ public class PreferencesHelper {
 	}
 
 	public static void setDeactivateManualEqualOnlyForThisTime(boolean b) {
+		if (prefs == null) {
+			return;
+		}
 		prefs.putBoolean(ASK_USER_DEACTIVATED_FOR_THIS_TIME, b);
 	}
 
 	public static boolean isDeactivateManualEqualOnlyForThisTime() {
+		if (prefs == null) {
+			return false;
+		}
 		return prefs.getBoolean(ASK_USER_DEACTIVATED_FOR_THIS_TIME, false);
 	}
 
 	public static boolean isAdaptConcurrently() {
+		if (prefs == null) {
+			return false;
+		}
 		return prefs.getBoolean(ADAPT_CONCURRENTLY, false);
 	}
 }
