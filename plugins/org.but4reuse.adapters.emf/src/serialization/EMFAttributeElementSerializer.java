6bb9cf473ba45579835243bee6c14d2d5363b11f
Maurice Frédéric Vincon <s8mevinc@stud.uni-saarland.de>
2023-03-14 10:38:25 +0100

    added javadoc for the EMF*ElementSerializer.java (for classes, attributes and references (AMEs)), that are used during the gsonExport

--- plugins/org.but4reuse.adapters.emf/src/serialization/EMFAttributeElementSerializer.java
+++ plugins/org.but4reuse.adapters.emf/src/serialization/EMFAttributeElementSerializer.java
@@ -10,8 +10,23 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
+/**
+ * This is a serializer for the gson export of EMFAttributeElements to be registered as TypeAdapter on the GsonBuilder
+ */
 public class EMFAttributeElementSerializer implements JsonSerializer<EMFAttributeElement> {
 
+	/**
+	 * Gson invokes this call-back method during serialization when it encounters a field of the specified type.
+	 * In the implementation of this call-back method, you should consider invoking
+	 * JsonSerializationContext.serialize(Object, Type) method to create JsonElements for any non-trivial field
+	 * of the src object. However, you should never invoke it on the src object itself since that will cause
+	 * an infinite loop (Gson will call your call-back method again).
+	 *
+	 * @param attributeElement to be serialized for the gsonExport; The object that needs to be converted to Json
+	 * @param type the actual type (fully genericized version) of the source object
+	 * @param context isn't used here
+	 * @return a JsonElement corresponding to the specified object
+	 */
 	@Override
 	public JsonElement serialize(EMFAttributeElement attributeElement, Type type, JsonSerializationContext context) {
 		final JsonObject jsonObject = new JsonObject();
